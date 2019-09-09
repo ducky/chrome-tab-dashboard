4eb0d4a9a8c53dbce7b3a1a2e35605cca7e490d9
Chris Tieman <iam@christieman.com>
2019-09-09 03:11:26 -0400

    Updated storage to play nice with chrome storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -5,12 +5,36 @@ const hasChromeStorage = !isEmpty(chrome) && !isEmpty(chrome.storage);
 export const get = (key, defaultResponse) => {
   return new Promise(resolve => {
     if (hasChromeStorage) {
-      chrome.storage.sync.get(key, item => {
-        resolve(item);
+      chrome.storage.sync.get([key], item => {
+        // eslint-disable-next-line
+        console.log(`Loading ${key} from Chrome Storage`);
+
+        if (item[key]) {
+          const data = JSON.parse(item[key]);
+          // eslint-disable-next-line
+          console.log(`Loaded ${key} from Chrome Storage`, data);
+          resolve(data);
+        } else {
+          // eslint-disable-next-line
+          console.log(`Failed to Load ${key} from Chrome Storage; Loading Default`);
+          resolve(defaultResponse);
+        }
       });
     } else {
+      // eslint-disable-next-line
+      console.log(`Loading ${key} from Local Storage`);
       const item = localStorage.getItem(key);
-      resolve(item ? JSON.parse(item) : defaultResponse);
+
+      if (item) {
+        const data = JSON.parse(item);
+        // eslint-disable-next-line
+        console.log(`Loaded ${key} from Local Storage`, data);
+        resolve(data);
+      } else {
+        // eslint-disable-next-line
+        console.log(`Failed to Load ${key} from Local Storage; Loading Default`);
+        resolve(defaultResponse);
+      }
     }
   });
 };
@@ -18,10 +42,14 @@ export const get = (key, defaultResponse) => {
 export const set = (key, data = {}) => {
   return new Promise(resolve => {
     if (hasChromeStorage) {
-      chrome.storage.sync.set({ key: data }, function() {
+      chrome.storage.sync.set({ [key]: JSON.stringify(data) }, () => {
+        // eslint-disable-next-line
+        console.log(`Saving ${key} to Chrome Storage`);
         resolve(data);
       });
     } else {
+      // eslint-disable-next-line
+      console.log(`Saving ${key} to Local Storage`);
       localStorage.setItem(key, JSON.stringify(data));
       resolve(data);
     }
